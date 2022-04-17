71c3fc2d891d0cd9d0d52a59f5d278d4eb7d6e6f
srv-pranjal <prns366@gmail.com>
2022-04-18 00:27:35 +0530

    feat: added redirect to category onclick

--- src/Components/Category/CategoryCard.js
+++ src/Components/Category/CategoryCard.js
@@ -1,9 +1,20 @@
+import { useProducts } from "contexts";
 import React from "react";
 import { Link } from "react-router-dom";
 
 export const CategoryCard = ({ categoryName, image, altText }) => {
+  const { dispatch } = useProducts();
+
+  const redirectToCategory = (categoryName) => {
+    dispatch({ type: "CLEAR_ALL_FILTERS" });
+    dispatch({
+      type: "FILTER_BY_CATEGORY",
+      payload: categoryName,
+    });
+  };
+
   return (
-    <Link to="/products">
+    <Link to="/products" onClick={() => redirectToCategory(categoryName)}>
       <article className="card card--vertical card--shadow">
         <div className="card__content">
           <div className="card__img-container">
